fb81e549698f68b2863938d24847190654bcb16a
Xiangyu Hu <xiangyu.hu@tum.de>
2026-06-04 11:03:35 +0000

    all 2d tests

--- tests/test_simulation/test_2d_simulation/simulation.cpp
+++ tests/test_simulation/test_2d_simulation/simulation.cpp
@@ -8,7 +8,7 @@
  */
 #include "sph_simulation.h"
 #include <gtest/gtest.h>
-/*
+
 TEST(simulations, dambreak)
 {
     SPH::SPHSimulation sim("./input/dambreak.json");
@@ -41,7 +41,7 @@ TEST(simulations, milling)
     sim.initializeSimulation();
     sim.run();
 }
-*/
+
 TEST(simulations, heat_transfer)
 {
     SPH::SPHSimulation sim("./input/heat_transfer.json");
